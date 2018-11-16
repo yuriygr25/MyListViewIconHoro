19ae21070c013652f541f6ea04d25126d49133ed
Yuriy Ivanov <yuriygr@yahoo.com>
2018-11-16 08:50:41 +0200

    commit

--- app/src/main/java/com/example/yura/mylistviewiconhoro/MainActivity.java
+++ app/src/main/java/com/example/yura/mylistviewiconhoro/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.yura.mylistviewiconhoro;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 //        Intent intent = new Intent(MainActivity.this, ListViewWithIconsActivity.class);
 
 //        startActivity(intent);
